ebb4fe70dd7a0944d8844616b5c83c467cb3c595
USER <EMAIL>
2021-11-18 20:20:05 +0700

    makes sign up page

---
+++ pages/index.vue
@@ -0,0 +1,177 @@
+<template>
+  <div>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <div
+          class="pa-7 white rounded-circle d-inline-block"
+        >
+          <!--<v-icon large color="blue">
+             mdi-cloud-upload
+          </v-icon>-->
+          <i class="fas fa-university" style="color: rgb(0, 162, 255); font-size:40px;" />
+        </div>
+      </v-col>
+    </v-row>
+    <h1 style="margin-top:20px;">
+      KNN BANK
+    </h1>
+    <h3>MOBLIE APP</h3>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="6"
+            md="4"
+          >
+            <v-menu transition="scroll-y-transition">
+              <template #activator="{ on, attrs }">
+                <v-text-field
+                  label="Please enter your password"
+                  placeholder="Password"
+                  filled
+                  rounded
+                  dense
+                  v-bind="attrs"
+                  v-on="on"
+                />
+              </template>
+              <div class="box">
+                <v-row>
+                  <v-col class="d-flex justify-center">
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      1
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      2
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      3
+                    </v-btn>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="d-flex justify-center ">
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      4
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      5
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      6
+                    </v-btn>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="d-flex justify-center ">
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      7
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      8
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      9
+                    </v-btn>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="d-flex justify-center ">
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      0
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="warning"
+                    >
+                      <i class="fas fa-times" />
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </div>
+            </v-menu>
+            <v-btn class="btn1">
+              <span class="txt1">LOG IN</span>
+            </v-btn>
+            <p>Forgot Password?</p>
+            <p>New To Bank Apps? Sing Up</p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
+</template>
+<style>
+.pa-7{
+  margin-top: 100px;
+}
+h3{
+  margin-bottom: 50px;
+}
+.txt1{
+  color: brown;
+}
+.btn1{
+  background: linear-gradient(to bottom,#ffeb3b,#ffc107);
+  margin-bottom: 60px;
+}
+.box{
+  background-color: white;
+  padding: 5px;
+  border-radius: 20px;
+}
+
+</style>
---
+++ pages/signup.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="box">
+    <v-form>
+      <p>Sign up your bank account</p>
+      <v-text-field
+        label="Your Name"
+        outlined
+        dense
+        style="padding: 0px 20px 0px 20px;"
+      />
+      <v-text-field
+        label="Bank Account"
+        outlined
+        dense
+        style="padding: 0px 20px 0px 20px;"
+      />
+      <v-text-field
+        label="Moblie Phone Number"
+        outlined
+        dense
+        style="padding: 0px 20px 0px 20px;"
+      />
+      <v-text-field
+        label="Email"
+        outlined
+        dense
+        style="padding: 0px 20px 0px 20px;"
+      />
+      <v-text-field
+        label="Password"
+        outlined
+        dense
+        style="padding: 0px 20px 10px 20px;"
+        append-icon="mdi-eye"
+      />
+      <div class="mess">
+        Use 6 characters with a mix of numbers and letters
+      </div>
+      <v-checkbox
+        label="By signning up, you agree to bank's term of User & Privacy Policy."
+        color="blue darken-3"
+      />
+      <v-row>
+        <v-col class="d-flex justify-center ">
+          <v-btn class="btn1">
+            <span class="txt1">Sign Up</span>
+          </v-btn>
+          <p> or </p>
+          <v-btn
+            class="ma-2"
+            outlined
+            color="indigo"
+          >
+            CANCLE
+          </v-btn>
+        </v-col>
+      </v-row>
+      <div class="signed">
+        Already signed up? <a href="#" class="log">Log in</a>
+      </div>
+    </v-form>
+  </div>
+</template>
+
+<style>
+
+.box{
+  background-color: rgb(255, 255, 255);
+  border-radius: 10px;
+  margin: 20px;
+  font-family: sans-serif;
+}
+p{
+  color: rgb(56, 142, 255);
+  padding: 20px;
+
+}
+.mess{
+  color: rgb(56, 142, 255);
+  font-family: sans-serif;
+}
+.signed{
+  color: rgb(56, 142, 255);
+  font-family: sans-serif;
+}
+.v-text-field--outlined fieldset{
+  color: #27272c !important;
+}
+.v-text-field--outlined label{
+  color: #313136b0 !important;
+}
+.v-checkbox--label{
+  color: #27272c !important;
+}
+.btn1{
+  background: linear-gradient(to bottom,#ffeb3b,#ffc107);
+  margin-top: 7px;
+}
+.txt1{
+  color: black;
+  font-family: sans-serif;
+}
+.log{
+  color: cornflowerblue;
+}
+
+</style>
